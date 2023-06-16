5532286a50bafb467376ea0f193eb57d7c28bab0
usmanghani2023 <usman.efulife@gmail.com>
2023-06-16 10:07:02 -0700

    my first repo

---
+++ first_assignment.dart
@@ -0,0 +1,12 @@
+void main() {
+  var alphabet = 'a';
+  if (alphabet == 'i' ||
+      alphabet == 'e' ||
+      alphabet == 'o' ||
+      alphabet == 'u' ||
+      alphabet == 'a') {
+    print('this is vowel word');
+  } else {
+    print('this is consonant word');
+  }
+}
